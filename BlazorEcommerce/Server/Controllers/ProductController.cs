5c24443f04ae82e97108be4f286b20ee39d484f2
Maksymilian Leone <maks.leone@gmail.com>
2023-11-17 10:36:56 +0100

    - implementing search component

--- BlazorEcommerce/Server/Controllers/ProductController.cs
+++ BlazorEcommerce/Server/Controllers/ProductController.cs
@@ -20,5 +20,33 @@ namespace BlazorEcommerce.Server.Controllers
             var result = await _productService.GetProductAsync();
             return Ok(result);
         }
+
+        [HttpGet("{productId}")]
+        public async Task<ActionResult<ServiceResponse<Product>>> GetProducts(int productId)
+        {
+            var result = await _productService.GetProductAsync(productId);
+            return Ok(result);
+        }
+
+        [HttpGet("category/{categoryUrl}")]
+        public async Task<ActionResult<ServiceResponse<Product>>> GetProductsByCategory(string categoryUrl)
+        {
+            var result = await _productService.GetProductByCategory(categoryUrl);
+            return Ok(result);
+        }
+
+        [HttpGet("search/{searchText}")]
+        public async Task<ActionResult<ServiceResponse<Product>>> SearchProducts(string searchText)
+        {
+            var result = await _productService.SearchProducts(searchText);
+            return Ok(result);
+        }
+
+        [HttpGet("searchsuggestions/{searchText}")]
+        public async Task<ActionResult<ServiceResponse<Product>>> GetProductSearchSuggestions(string searchText)
+        {
+            var result = await _productService.GetProductSearchSuggestions(searchText);
+            return Ok(result);
+        }
     }
 }
--- BlazorEcommerce/Server/Services/ProductService/IProductService.cs
+++ BlazorEcommerce/Server/Services/ProductService/IProductService.cs
@@ -3,5 +3,11 @@
     public interface IProductService
     {
         Task<ServiceResponse<List<Product>>> GetProductAsync();
+        Task<ServiceResponse<Product>> GetProductAsync(int productId);
+        Task<ServiceResponse<List<Product>>> GetProductByCategory(string categoryUrl);
+        Task<ServiceResponse<List<Product>>> SearchProducts(string searchText);
+        Task<ServiceResponse<List<string>>> GetProductSearchSuggestions(string searchText);
+
+
     }
 }
