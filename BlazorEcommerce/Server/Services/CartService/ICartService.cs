cddc3460406d21c3f49bb9668afac63eaec9d3de
Maksymilian Leone <maks.leone@gmail.com>
2023-11-23 14:43:39 +0100

    Store Cart Items using the Database

--- BlazorEcommerce/Server/Services/CartService/ICartService.cs
+++ BlazorEcommerce/Server/Services/CartService/ICartService.cs
@@ -1,7 +1,10 @@
-﻿namespace BlazorEcommerce.Server.Services.CartService
+﻿using BlazorEcommerce.Shared;
+
+namespace BlazorEcommerce.Server.Services.CartService
 {
     public interface ICartService
     {
         Task<ServiceResponse<List<CartProductResponse>>> GetCartProducts(List<CartItem> cartItems);
+        Task<ServiceResponse<List<CartProductResponse>>> StoreCartItems(List<CartItem> cartItems, int userId);
     }
 }
