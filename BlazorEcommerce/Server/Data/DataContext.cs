7c1e9ad81ade3413ff0ee11a85bfdb4f0a377470
Maksymilian Leone <maks.leone@gmail.com>
2023-12-18 14:04:26 +0100

    Saving progress before VS update

--- BlazorEcommerce/Server/Data/DataContext.cs
+++ BlazorEcommerce/Server/Data/DataContext.cs
@@ -76,7 +76,7 @@
                 Id = 3,
                 Title = "Nineteen Eighty-Four",
                 Description = "Nineteen Eighty-Four (also stylised as 1984) is a dystopian social science fiction novel and cautionary tale written by English writer George Orwell. It was published on 8 June 1949 by Secker & Warburg as Orwell's ninth and final book completed in his lifetime. Thematically, it centres on the consequences of totalitarianism, mass surveillance and repressive regimentation of people and behaviours within society.[2][3] Orwell, a democratic socialist, modelled the totalitarian government in the novel after Stalinist Russia and Nazi Germany.[2][3][4] More broadly, the novel examines the role of truth and facts within politics and the ways in which they are manipulated.",
-                ImageURL = "https://upload.wikimedia.org/wikipedia/commons/c/c3/1984first.jpg",
+                ImageURL = "https://upload.wikimedia.org/wikipedia/en/a/ac/Saving_Private_Ryan_poster.jpg?20160109123913",
                 CategoryId = 1
             },
             new Product
--- BlazorEcommerce/Server/Services/CartService/ICartService.cs
+++ BlazorEcommerce/Server/Services/CartService/ICartService.cs
@@ -1,13 +1,11 @@
-﻿using BlazorEcommerce.Shared;
-
-namespace BlazorEcommerce.Server.Services.CartService
+﻿namespace BlazorEcommerce.Server.Services.CartService
 {
     public interface ICartService
     {
         Task<ServiceResponse<List<CartProductResponse>>> GetCartProducts(List<CartItem> cartItems);
         Task<ServiceResponse<List<CartProductResponse>>> StoreCartItems(List<CartItem> cartItems);
         Task<ServiceResponse<int>> GetCartItemsCount();
-        Task<ServiceResponse<List<CartProductResponse>>> GetDbCartProducts();
+        Task<ServiceResponse<List<CartProductResponse>>> GetDbCartProducts(int? userId = null);
         Task<ServiceResponse<bool>> AddToCart(CartItem cartItem);
         Task<ServiceResponse<bool>> UpdateQuantity(CartItem cartItem);
         Task<ServiceResponse<bool>> RemoveItemFromCart(int productId, int productTypeId);
